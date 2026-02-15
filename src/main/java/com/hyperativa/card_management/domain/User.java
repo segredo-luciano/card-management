b9a2c58bc04dce67e61a255da8cba58c9c4489e9
segredo-luciano <lucianosegredo20@gmail.com>
2026-02-15 17:03:40 -0300

    adjusts on User entity

--- src/main/java/com/hyperativa/card_management/domain/User.java
+++ src/main/java/com/hyperativa/card_management/domain/User.java
@@ -8,14 +8,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user")
 public class User {
 
