af696416b6957f6b3cd0f3c4549dad30b1eaf152
segredo-luciano <lucianosegredo20@gmail.com>
2026-02-16 17:35:02 -0300

    adding bad request exception return

--- src/main/java/com/hyperativa/card_management/api/dto/request/CreateCardRequest.java
+++ src/main/java/com/hyperativa/card_management/api/dto/request/CreateCardRequest.java
@@ -1,5 +1,5 @@
 package com.hyperativa.card_management.api.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CreateCardRequest(Long batchId, Integer orderNumber, @NotBlank(message = "cardNumber is required") String cardNumber) { }
+public record CreateCardRequest(Long batchId, Integer orderNumber, @NotNull(message = "cardNumber is required") String cardNumber) { }
--- src/main/java/com/hyperativa/card_management/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hyperativa/card_management/api/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.hyperativa.card_management.api.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,6 +24,22 @@ public class GlobalExceptionHandler {
                 "Parâmetro obrigatório ausente: " + ex.getParameterName()
         );
     }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+          .getFieldErrors()
+          .forEach(error ->
+              errors.put(error.getField(), error.getDefaultMessage())
+          );
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -29,6 +50,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponse handleBadCredentials(BadCredentialsException bce) {
+        return new ErrorResponse(bce.getMessage());
+    }
+    
+    @ExceptionHandler(CardNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleBadCredentials(CardNotFoundException bce) {
+        return new ErrorResponse(bce.getMessage());
+    }
+    
+    @ExceptionHandler(CardAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleBadCredentials(CardAlreadyExistsException bce) {
+        return new ErrorResponse(bce.getMessage());
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadCredentials(IllegalArgumentException bce) {
         return new ErrorResponse(bce.getMessage());
     }
 }
